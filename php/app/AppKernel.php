c93607347b53a78b63feb5531cee4501d32b4ef3
Jesús Flores <jesus.flores@bqreaders.com>
2014-06-02 15:55:23 +0200

    Revert "Add Jfs bundle and update composer"
    This reverts commit a30c72626f4ea192108a767e37a5602ed242ec3b.

--- php/app/AppKernel.php
+++ php/app/AppKernel.php
@@ -16,11 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Jfs\CrawlerAdminBundle\JfsCrawlerAdminBundle(),
-            new Jfs\UserBundle\JfsUserBundle(),
-            new Jfs\ApiBundle\JfsApiBundle(),
-            /*new Bmatzner\FoundationBundle\BmatznerFoundationBundle(),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),*/
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
